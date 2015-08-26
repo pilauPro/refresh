46e53df3deb4023c59ce851803960e4f9db3bbfe
pilauPro <leiningem001@hawaii.rr.com>
2015-08-25 17:46:27 -1000

    user defined grid added

--- js/refresher.js
+++ js/refresher.js
@@ -1,5 +1,14 @@
 var main = function(){
-	alert("Loaded, buddy");
+
+	var rows = prompt("how many rows?")
+	var squares = prompt("how many squares per row?")
+
+	for (var i = 0; i < rows; i++){
+		$('h1').after('<div class=\'row\'></div>')
+	}
+	for (var i = 0; i < squares; i++){
+		$('.row').append('<div class=\'square\'></div>')
+	}
 }
 
 $(document).ready(main)
