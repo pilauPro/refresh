faeb389948f2647408289f474dfad44ce7235e86
pilauPro <leiningem001@hawaii.rr.com>
2015-08-26 15:35:59 -1000

    box size dependent on user input and mouseenter colors box

--- js/refresher.js
+++ js/refresher.js
@@ -2,13 +2,24 @@ var main = function(){
 
 	var rows = prompt("how many rows?")
 	var squares = prompt("how many squares per row?")
+	var width = 1200 / squares
+	var height = 600 / rows
 
 	for (var i = 0; i < rows; i++){
 		$('h1').after('<div class=\'row\'></div>')
 	}
+
 	for (var i = 0; i < squares; i++){
 		$('.row').append('<div class=\'square\'></div>')
 	}
+	$('.square').css({
+		width: width,
+		height: height
+	});
+
+	$('.square').mouseenter(function(){
+		$(this).css('background-color', 'green');
+	});
 }
 
 $(document).ready(main)
