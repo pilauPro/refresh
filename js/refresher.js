61b6e6755d751a7b7d49e31d82e9e6741237ca90
pilauPro <leiningem001@hawaii.rr.com>
2015-08-28 17:41:07 -1000

    menu bar functional

--- js/refresher.js
+++ js/refresher.js
@@ -1,30 +1,28 @@
 var main = function(){
-
-	var rows = prompt("how many rows?")
-	var squares = prompt("how many squares per row?")
-	var width = 1200 / squares
-	var height = 600 / rows
-
-	for (var i = 0; i < rows; i++){
-		$('.menu').after('<div class=\'row\'></div>')
-	}
-
-	for (var i = 0; i < squares; i++){
-		$('.row').append('<div class=\'square\'></div>')
-	}
-	$('.square').css({
-		width: width,
-		height: height
-	});
-
-	$('.square').mouseenter(function(){
-		$(this).css('background-color', 'green');
-	});
-
 	$('#draw').click(function(){
-		alert("hi");
-	});
+		// alert($('input:radio[name=format]:checked').val())
+		var rows = $('input:text[name=rows]').val();
+		var elements = $('input:text[name=elements]').val();
 
+		var width = 1400 / elements
+		var height = 800 / rows
+
+		for (var i = 0; i < rows; i++){
+			$('.menu').after('<div class=\'row\'></div>')
+		}
+
+		for (var i = 0; i < elements; i++){
+			$('.row').append('<div class=\'square\'></div>')
+		}
+
+		$('.square').css({
+			width: width,
+			height: height
+		});
+		$('.square').mouseenter(function(){
+			$(this).css('background-color', 'green')
+		});
+	});
 }
 
 $(document).ready(main)
