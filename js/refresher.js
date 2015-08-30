4dd55a4bf77ee3fe87ff48a039ee91568ebb559d
pilauPro <leiningem001@hawaii.rr.com>
2015-08-30 10:22:44 -1000

    user specified element shape

--- js/refresher.js
+++ js/refresher.js
@@ -1,11 +1,12 @@
 var main = function(){
 	$('#draw').click(function(){
-		// alert($('input:radio[name=format]:checked').val())
+		var shape = $('input:radio[name=shape]:checked').val()
 		var rows = $('input:text[name=rows]').val();
 		var elements = $('input:text[name=elements]').val();
 
-		var width = 1400 / elements
+		var width = 1300 / elements
 		var height = 800 / rows
+
 
 		for (var i = 0; i < rows; i++){
 			$('.menu').after('<div class=\'row\'></div>')
@@ -19,6 +20,15 @@ var main = function(){
 			width: width,
 			height: height
 		});
+
+		if (shape == 'squares'){
+			$('.square').css('border-radius', '5%');
+		}
+		else if (shape = 'circles'){
+			$('.square').css('border-radius', '100%');
+		}
+
+
 		$('.square').mouseenter(function(){
 			$(this).css('background-color', 'green')
 		});
