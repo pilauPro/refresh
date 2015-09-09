af7fa64d5f82c3932d33ac9c0d0b13b3efe9678f
pilauPro <leiningem001@hawaii.rr.com>
2015-09-08 17:56:47 -1000

    opacity and control button animations

--- js/shapes.js
+++ js/shapes.js
@@ -4,14 +4,18 @@ var main = function(){
 		dex = $(this).index()
 		square = $('.square').eq(dex)
 
+
 		if (! (square.hasClass('downSquare') || square.hasClass('upSquare'))){
+			$(this).addClass('activeControl');
 			square.addClass('downSquare');
 		}
 		else if (square.hasClass('downSquare') && !square.hasClass('upSquare')){
 			square.addClass('upSquare');
+			$(this).addClass('deactiveControl');
 		}
 		else{
 			square.removeClass('upSquare');
+			$(this).removeClass('deactiveControl');
 		}
 	})
 }
