99ef9d6b4bbbf42864100bf6a1520210d8bc1535
pilauPro <leiningem001@hawaii.rr.com>
2015-09-21 17:27:55 -1000

    finally got styles to persist throughout animations

--- js/transform.js
+++ js/transform.js
@@ -1,9 +1,25 @@
-$('#trans-shape').click(function(){
-	$(this).addClass('colorShape');
-	$(this).one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(e) {
-		$(this).addClass('resizeShape');
-		$(this).one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(e) {
-			$(this).addClass('rotateShape');
-		});
-    });
+// $('#trans-shape').click(function(){
+// 	$(this).addClass('rotateShape');
+// 	$(this).one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(e) {
+// 		$(this).addClass('resizeShape');
+// 		$(this).one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function() {
+// 			$(this).addClass('rotateShape');
+// 		});
+//     });
+// })
+
+$('#colorBtn').click(function(){
+	var shape = $('#trans-shape')
+
+	shape.addClass('colorShape');
+})
+$('#sizeBtn').click(function(){
+	var shape = $('#trans-shape')
+	shape.removeClass('colorShape');
+	shape.addClass('resizeShape');
+})
+$('#rotateBtn').click(function(){
+	var shape = $('#trans-shape')
+	shape.removeClass('resizeShape');
+	shape.addClass('rotateShape');
 })
