124c079ea822068b678180cc23187e56f6a25c03
pilauPro <leiningem001@hawaii.rr.com>
2015-09-22 16:39:35 -1000

    keyframes tinkering continues

--- js/transform.js
+++ js/transform.js
@@ -1,25 +1,11 @@
-// $('#trans-shape').click(function(){
-// 	$(this).addClass('rotateShape');
-// 	$(this).one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(e) {
-// 		$(this).addClass('resizeShape');
-// 		$(this).one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function() {
-// 			$(this).addClass('rotateShape');
-// 		});
-//     });
-// })
-
-$('#colorBtn').click(function(){
+$('#animateBtn').click(function(){
 	var shape = $('#trans-shape')
 
 	shape.addClass('colorShape');
-})
-$('#sizeBtn').click(function(){
-	var shape = $('#trans-shape')
-	shape.removeClass('colorShape');
-	shape.addClass('resizeShape');
-})
-$('#rotateBtn').click(function(){
-	var shape = $('#trans-shape')
-	shape.removeClass('resizeShape');
-	shape.addClass('rotateShape');
+	shape.one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function() {
+		shape.addClass('resizeShape');
+		shape.one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function() {
+			shape.addClass('rotateShape');
+		});
+    });
 })
