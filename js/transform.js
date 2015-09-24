8d96befca4c6b07702db915fdff455974ca6a632
pilauPro <leiningem001@hawaii.rr.com>
2015-09-23 15:48:48 -1000

    keyframes animations 2

--- js/transform.js
+++ js/transform.js
@@ -1,6 +1,14 @@
 $('#animateBtn').click(function(){
 	var shape = $('#trans-shape')
 
+	if (shape.hasClass('rotateShape')){
+		animate2(shape);
+	}
+	else{
+		animate1(shape);
+	}
+})
+function animate1(shape){
 	shape.addClass('colorShape');
 	shape.one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function() {
 		shape.addClass('resizeShape');
@@ -8,4 +16,21 @@ $('#animateBtn').click(function(){
 			shape.addClass('rotateShape');
 		});
     });
-})
+}
+
+function animate2(shape){
+	shape.addClass('justRotate');
+	shape.one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function() {
+		shape.removeClass('justRotate');
+	});
+	// if (!shape.hasClass('justRotate')){
+	// 	shape.addClass('justRotate');
+	// }
+	// else {
+	// 	(shape.hasClass('justRotate'))
+	// 	shape.removeClass('justRotate');
+	// 	shape.one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function() {
+	// 		shape.addClass('justRotate');
+	// 	});
+	// }
+}
